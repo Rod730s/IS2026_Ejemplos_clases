4f835ce0f20efa40cc4dc7331d674f3ee623d1e7
Rod730s <scorp730s@gmail.com>
2025-11-11 15:40:07 -0600

    init: Creando repo

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let x = 10;
+x = 100;
+console.log(x);
+
+//Iteraciones
+
+const cielo = document.getElementById("cielo");
+
+for(let i=0; i<5; i++){
+
+    cielo.innerHTML += '🌟';
+}
+
+const planetas = ["🌎","🪐","🌕"]
+
+for(const planeta of planetas){
+    cielo.innerHTML += planeta;
+}
+
+let n=0
+while (n<5){
+    cielo.innerHTML += "☁️";
+    n++
+}
+
+let m = 0;
+do{
+    cielo.innerHTML += "🚀";
+    m++
+} while (m<4)
+
+const btnEstrella = document.getElementById("btnEstrella");
+
+btnEstrella.addEventListener("click", ()=> {
+    cielo.innerHTML += "🌟";
+})
